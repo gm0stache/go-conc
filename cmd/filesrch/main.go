7f3b51aef23072454afe0377f70cbc103e8949ef
Gabriel <45315755+gm0stache@users.noreply.github.com>
2024-02-06 07:59:52 +0100

    feat(filesrch): sort results

--- cmd/filesrch/main.go
+++ cmd/filesrch/main.go
@@ -3,11 +3,38 @@ package main
 import (
 	"errors"
 	"fmt"
-	sync "github.com/gm0stache/go-conc/internal/sync"
 	"os"
 	"path"
+	"sort"
 	"strings"
+	stdsync "sync"
+
+	sync "github.com/gm0stache/go-conc/internal/sync"
 )
+
+type asyncStore[T any] struct {
+	values []T
+	mtx    *stdsync.Mutex
+}
+
+func NewStore[T any]() *asyncStore[T] {
+	return &asyncStore[T]{
+		values: []T{},
+		mtx:    &stdsync.Mutex{},
+	}
+}
+
+func (as *asyncStore[T]) append(el T) {
+	as.mtx.Lock()
+	defer as.mtx.Unlock()
+	as.values = append(as.values, el)
+}
+
+func (as *asyncStore[T]) getVals() []T {
+	as.mtx.Lock()
+	defer as.mtx.Unlock()
+	return as.values
+}
 
 func main() {
 	if len(os.Args) != 3 { // index 0 is always the program name, so here 'filesrch'
@@ -19,11 +46,16 @@ func main() {
 	wg := sync.NewWaitGroup()
 	wg.Add(1)
 
-	searchFile(dir, filename, wg) // does the 'go' here make a difference
+	store := NewStore[string]()
+	searchFile(dir, filename, store, wg) // does the 'go' here make a difference
 	wg.Wait()
+
+	paths := store.getVals()
+	sort.Strings(paths)
+	print(strings.Join(paths, "\n"))
 }
 
-func searchFile(dir string, filename string, wg *sync.WaitGroup) {
+func searchFile(dir string, filename string, store *asyncStore[string], wg *sync.WaitGroup) {
 	files, err := os.ReadDir(dir)
 	if err != nil {
 		printErrAndExit(err)
@@ -33,10 +65,10 @@ func searchFile(dir string, filename string, wg *sync.WaitGroup) {
 		fullpath := path.Join(dir, f.Name())
 		if f.IsDir() {
 			wg.Add(1)
-			go searchFile(fullpath, filename, wg)
+			go searchFile(fullpath, filename, store, wg)
 		}
 		if strings.Contains(f.Name(), filename) {
-			fmt.Println(fullpath)
+			store.append(fullpath)
 		}
 	}
 
